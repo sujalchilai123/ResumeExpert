05b7714dc2367b7848e05b47eefdaebe163d6898
Sujal Chilai <sujal.chilai7@gmail.com>
2025-10-09 00:48:52 +0530

    used the deployed backend

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,22 +1,21 @@
-export const BASE_URL = "https://resumeexpert-backend.onrender.com";
+// utils/apiPaths.js
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-//utils/apiPath.js
 export const API_PATHS = {
-
-    AUTH: {
-        REGISTER: "/api/auth/register",
-        LOGIN: "/api/auth/login",
-        GET_PROFILE: "/api/auth/profile",
-    },
-    RESUME: {
-        CREATE: "/api/resume",
-        GET_ALL: "/api/resume",
-        GET_BY_ID: (id) => `/api/resume/${id}`,
-        UPDATE: (id) => `/api/resume/${id}`,
-        DELETE: (id) => `/api/resume/${id}`,
-        UPLOAD_IMAGES: (id) => `/api/resume/${id}/upload-images`,
-    },
-    image: {
-        UPLOAD_IMAGE: "api/auth/upload-image",
-    },
+  AUTH: {
+    REGISTER: `${BASE_URL}/api/auth/register`,
+    LOGIN: `${BASE_URL}/api/auth/login`,
+    GET_PROFILE: `${BASE_URL}/api/auth/profile`,
+  },
+  RESUME: {
+    CREATE: `${BASE_URL}/api/resume`,
+    GET_ALL: `${BASE_URL}/api/resume`,
+    GET_BY_ID: (id) => `${BASE_URL}/api/resume/${id}`,
+    UPDATE: (id) => `${BASE_URL}/api/resume/${id}`,
+    DELETE: (id) => `${BASE_URL}/api/resume/${id}`,
+    UPLOAD_IMAGES: (id) => `${BASE_URL}/api/resume/${id}/upload-images`,
+  },
+  IMAGE: {
+    UPLOAD_IMAGE: `${BASE_URL}/api/auth/upload-image`,
+  },
 };
